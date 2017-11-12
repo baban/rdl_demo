2f822759906d3d2a79807570a2421b8995a6ff3e
masanao.matsubara <m_matsubara@okwave.co.jp>
2017-11-12 16:21:54 +0900

    userを作成

--- app/controllers/event_entries_controller.rb
+++ app/controllers/event_entries_controller.rb
@@ -1,5 +1,6 @@
 class EventEntriesController < ApplicationController
   def create
+    Event.find(params[:event_id]).event_entries.create(user: current_user)
     redirect_to :events
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171112064557) do
+ActiveRecord::Schema.define(version: 20171112072002) do
 
   create_table "event_entries", force: :cascade do |t|
     t.integer "event_id"
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 20171112064557) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
